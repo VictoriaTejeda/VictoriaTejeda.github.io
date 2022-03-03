4a33c7b64a100aa8e77310f02f004037cdab04f2
Victoria Tejeda <vicyatm@gmail.com>
2022-03-03 01:15:39 -0600

    dynamic funcionality

--- index.js
+++ index.js
@@ -1,38 +1,26 @@
-const basic = document.getElementById("basic");
-const middle = document.getElementById("middle");
-const avance = document.getElementById("high");
-const btnBasic = document.getElementById("basicPlan");
-const btnMiddle = document.getElementById("middlePlan");
-const btnAvance = document.getElementById("avancePlan");
-const btnContinue = document.getElementById("continue");
+const prueba = (id, idPlan, idBtn ) => {
+  let allCards = document.querySelectorAll("div.card");
+  let allbtns = document.querySelectorAll(".btn-plan");
 
-
-const selectPlanBasic = () => {
-  basic.classList.add("selected-plan");
-  middle.classList.remove("selected-plan");
-  avance.classList.remove("selected-plan");
-  btnBasic.innerHTML = "Seleccionado";
-  btnMiddle.innerHTML = "Seleccionar este plan";
-  btnContinue.innerHTML = "Continuar con Básico";
-};
-
-const selectPlanInter = () => {
-  middle.classList.add("selected-plan");
-  basic.classList.remove("selected-plan");
-  avance.classList.remove("selected-plan");
-  btnMiddle.innerHTML = "Seleccionado";
-  btnBasic.innerHTML = "Seleccionar este plan";
-  btnAvance.innerHTML = "Seleccionar este plan";
-  btnContinue.innerHTML = "Continuar con Intermedio";
-};
-
-const selectPlanAvance = () => {
-    avance.classList.add("selected-plan");
-    basic.classList.remove("selected-plan");
-    middle.classList.remove("selected-plan");
-    btnAvance.innerHTML = "Seleccionado";
-    btnBasic.innerHTML = "Seleccionar este plan";
-    btnMiddle.innerHTML = "Seleccionar este plan";
-    btnContinue.innerHTML = "Continuar con Avanzado";
-  };
   
+  allCards.forEach(function (id) {
+    id.classList.remove("selected-plan");
+  });
+  allbtns.forEach(function (btn) {
+    btn.innerHTML = "seleccionar este plan";
+  });
+  
+
+  active(id, idPlan, idBtn );
+};
+
+const active = (val,idPlan, selected ) => {
+  let card = document.getElementById(val);
+  let btn = document.getElementById(selected);
+  let plan=document.getElementById(idPlan);
+  let btnContinue = document.getElementById("continue");
+  const type= plan.innerHTML
+  card.classList.add("selected-plan");
+  btn.innerHTML = "seleccionado";
+ btnContinue.innerHTML= `Continuar con  ${type}`
+};
